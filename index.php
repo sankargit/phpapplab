c53ec89fdc2b8f1d92f3c911284e73f8c2403146
tw.maelstrom@gmail.com <trwilson@a8206633e255.ant.amazon.com>
2014-08-27 02:39:51 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body
+{
+background-color:#f9f9f9;
+}
+h1
+{
+color:orange;
+text-align:center;
+}
+p
+{
+
+font-size:30px;
+text-align: center;
+}
+
+#content{
+	width: 400px;
+	margin:0px auto;
+	}
+#header{
+	margin-top: 50px;
+	margin-bottom: 50px;
+	}
+
+</style>
+</head>
+	<body>
+		<div id="content">
+			<div id="header"><img src="<<banner file to S3>>"></div>
+			<div id="main">
+				<p>
+
+# your PHP content goes here
+
+				</p>
+			</div>
+			<div id="footer"> </div>
+		</div>
+	</body>
+</html>
